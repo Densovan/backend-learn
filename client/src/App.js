3aadc5ad09f5a35194478138072b81a075ed07ef
Densovan <sarimramata@gmail.com>
2020-02-09 11:35:27 +0700

    add contact

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/navbar";
+import Home from "./components/pages/home";
+import About from "./components/pages/about";
+import ContactState from "./context/contact/contactState";
+import "./App.css";
 
 const App = () => {
-  return <div className="App">My app</div>;
+  return (
+    <ContactState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home}></Route>
+              <Route exact path="/about" component={About}></Route>
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
+  );
 };
 
 export default App;
---
+++ client/src/components/contacts/contacts.js
@@ -0,0 +1,17 @@
+import React, { Fragment, useContext } from "react";
+import ContactContext from "../../context/contact/contactContext";
+import ContactItem from "../contacts/contactItem";
+const Contacts = () => {
+  const contactContext = useContext(ContactContext);
+
+  const { contacts } = contactContext;
+  return (
+    <Fragment>
+      {contacts.map(contact => (
+        <ContactItem key={contact.id} contact={contact} />
+      ))}
+    </Fragment>
+  );
+};
+
+export default Contacts;
